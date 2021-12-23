a33844180a8e9becf085274e9793b1d57e640ff4
Maroders <DShishlov@gmail.com>
2021-12-23 13:27:30 +0300

    Class_1, Class_2, Class_3 finished

--- Class_3.py
+++ Class_3.py
@@ -1,2 +1,43 @@
-class Petroleum:
-    ...
+class GasStation:
+    def __init__(self, petroleum_type: str, number_of_cars: int):
+        """
+        Создает автозаправочную станцию
+
+        :param petroleum_type: тип топлива
+        :param number_of_cars: количество автомобилей
+        """
+        self.petroleum_type = petroleum_type
+        self.petroleum_amount = petroleum_amount
+        self.number_of_cars = number_of_cars
+
+    def petroleum_amount(self, amount: int) -> dict:
+        """
+        Функция, определяющая количество каждого типа топлива
+
+        :param amount: количество топлива
+        :return: словарь, в котором содержится информация по количеству каждого типа топлива
+        """
+        ...
+
+    def car_types(self, type: str) -> dict:
+        """
+        Функция, определяющая, какому количеству автомобилей требуется какое количество топлива
+
+        :param type: тип автомобиля
+        :return: словарь с указанием типа автомобиля, потребляемого топлива и его количества
+        """
+        ...
+
+    def queue(self, petroleum_amount: dict, car_types: dict) -> list:
+        """
+        Функция, формирующая очередь автомобилей на автозаправочной станции
+
+        :param petroleum_amount: Функция, определяющая количество каждого типа топлива
+        :param car_types: Функция, определяющая, какому количеству автомобилей требуется какое количество топлива
+        :return: список, обозначающий очередь автомобилей на АЗС
+        """
+        ...
+
+
+if __name__ == "__main__":
+    Station_1 = GasStation("Солярка", 50)
