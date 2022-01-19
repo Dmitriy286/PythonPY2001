a1d11a2d00d254c0ce8ac69c2a5d24857fe247f2
Maroders <DShishlov@gmail.com>
2022-01-19 20:24:18 +0300

    Homework_18.01.2022 task1_Date

--- Инкапсуляция, наследование, полиморфизм/Домашнее задание/task1_Date_property/main.py
+++ Инкапсуляция, наследование, полиморфизм/Домашнее задание/task1_Date_property/main.py
@@ -12,25 +12,76 @@ class Date:
 
         self.is_valid_date(self.day, self.month, self.year)
 
-    # TODO какой декоратор следует применить?
-    def is_leap_year(self, year: int) -> bool:
+    @staticmethod
+    def is_leap_year(year: int) -> bool:
         """Проверяет, является ли год високосным"""
-        ...  # TODO записать условие проверки високосного года
+        if year % 4 == 0:
+            return True
+        else:
+            return False# TODO записать условие проверки високосного года
 
     def get_max_day(self, month: int, year: int) -> int:
         """Возвращает максимальное количество дней в месяце для указанного года"""
-        ...  # TODO вернуть количество дней указанного месяца
+        if self.is_leap_year(year):
+            return self.DAY_OF_MONTH[1][month - 1]
+        else:
+            return self.DAY_OF_MONTH[0][month - 1]# TODO вернуть количество дней указанного месяца
 
     def is_valid_date(self, day: int, month: int, year: int) -> None:
         """Проверяет, является ли дата корректной"""
-        ...  # TODO если указанный набор день, месяц и год неверны, то вызвать ошибку ValueError
+        if day > self.get_max_day(month, year):
+            raise ValueError("???")# TODO если указанный набор день, месяц и год неверны, то вызвать ошибку ValueError
 
     # TODO записать getter и setter для дня
+    @property
+    def day(self):
+        return self._day
+
+    @day.setter
+    def day(self, day):
+        if not isinstance(day, int):
+            raise TypeError("Введено не число")
+
+        if not 0 < day <= 31:
+            raise ValueError("Выходит за границы диапазона 1 - 31")
+
+        self._day = day
+
+    @property
+    def month(self):
+        return self._month
+
+    @month.setter
+    def month(self, month):
+        if not isinstance(month, int):
+            raise TypeError("Введено не число")
+
+        if not 0 < month <= 12:
+            raise ValueError("Выходит за границы диапазона 1 - 12")
+
+        self._month = month
+
+    @property
+    def year(self):
+        return self._year
+
+    @year.setter
+    def year(self, year):
+        if not isinstance(year, int):
+            raise TypeError("Введено не число")
+
+        if year < 0:
+            raise ValueError("Введено число меньше 0")
+
+        self._year = year
 
     # TODO записать getter и setter для месяца
 
     # TODO записать getter и setter для года
 
+    def __str__(self):
+        return f"{self.__class__.__name__} ({self.day:<2}, {self.month:<2}, {self.year:<4})"
 
 if __name__ == "__main__":
-    ...
+    date_1 = Date(29, 2, 2021)
+    print(date_1)
