559245b1fadc89d4b4cfd667243bee797de40a7e
Maroders <DShishlov@gmail.com>
2022-01-25 20:51:13 +0300

    Lecture_4_homework

--- Паттерны/Домашнее задание/task1_Dict_iter_items/main.py
+++ Паттерны/Домашнее задание/task1_Dict_iter_items/main.py
@@ -1,8 +1,11 @@
-from typing import Iterator, Tuple, Hashable, Any
+from typing import Iterator, Tuple, Hashable, Any, Dict
 
 
-class MyDict(...):  # TODO Наследование от класса dict
-    ...  # TODO переопределить метод __iter__
+class MyDict(Dict):  # TODO Наследование от класса dict
+    def __iter__(self):
+        for i, j in zip(self.keys(), self.values()):
+            yield i, j
+        # TODO переопределить метод __iter__
 
 
 if __name__ == "__main__":
--- Паттерны/Домашнее задание/task2_LinkedList_Mutable_sequence/main.py
+++ Паттерны/Домашнее задание/task2_LinkedList_Mutable_sequence/main.py
@@ -1,5 +1,16 @@
 # TODO Реализовать LinkedList как MutableSequence
+from collections.abc import MutableSequence
+
+class LinkedList(MutableSequence):
+    ...
 
 
 if __name__ == "__main__":
-    ...
+    linked_list = LinkedList()
+    print(linked_list.__dict__)
+
+    # list_ = [1, 2, 3]
+    # linked_list = LinkedList(list_)
+    # print(linked_list)
+    #
+    # print(linked_list[1])
