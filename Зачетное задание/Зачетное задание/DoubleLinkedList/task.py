caf7d134744aae8228afbbbde719c4e6b99627ab
Maroders <DShishlov@gmail.com>
2022-01-31 22:34:03 +0300

    PY_200_Zachet

--- Зачетное задание/Зачетное задание/DoubleLinkedList/task.py
+++ Зачетное задание/Зачетное задание/DoubleLinkedList/task.py
@@ -5,16 +5,22 @@ from node import Node, DoubleLinkedNode
 
 
 class LinkedList(MutableSequence):
-    def __init__(self, data: Iterable):
+    def __init__(self, data: Iterable = None):
         # self.data = data
-        self.len = 0
+        self._len = 0
         self.head: Optional[Node] = None
         self.tail: Optional[Node] = None
 
-        if self is None:
-            for value in data:
-                self.append(value)
-                self.len += 1
+        for value in data:
+            self.append(value)
+
+    def __len__(self) -> int:
+        """
+        Метод устанавливает длину связанного списка
+        :return: целое число
+        """
+        self._len = len(self)
+        return self._len
 
     def __iter__(self) -> Iterator:
         """
@@ -24,24 +30,30 @@ class LinkedList(MutableSequence):
         return self
 
     @staticmethod
-    def linked_nodes(left_node: "Node", right_node: "Node"):
+    def linked_nodes(left_node: "Node", right_node: Optional["Node"] = None):
         left_node.next = right_node
 
     def index_valid(self, index):
         if not isinstance(index, int):
             raise TypeError("Передано не число")
 
-        if index < 0 or index >= self.len:
+        if index < 0 or index >= self._len:
             raise ValueError("Переданное число выходит за границы допустимого диапазона")
+
+        return index
 
     def step_by_step_for_node(self, index):
         """
-        Метод возвращает ноду  по индексу
+        Метод возвращает ноду по индексу
         :param index: индекс, по которому необходимо вернуть ноду
         :return:
         """
         self.index_valid(index)
-        return self[index]
+        current_node = self.head
+        for _ in range(index):
+            current_node = current_node.next
+
+        return current_node
 
     def __getitem__(self, index: int) -> Any:
         """
@@ -71,18 +83,62 @@ class LinkedList(MutableSequence):
         """
         self.index_valid(index)
         delited_node = self.step_by_step_for_node(index)
+        previous_node = self.step_by_step_for_node(index - 1)
+        next_node = self.step_by_step_for_node(index + 1)
+        self.linked_nodes(previous_node, next_node)
+        delited_node.next = None
+        self._len -= 1
 
+    def append(self, value: Any):
+        if self._len == 0:
+            append_node = Node(value)
+            self.head = self.tail = append_node
 
-    def __len__(self) -> int:
-        pass
+        else:
+            append_node = Node(value)
+            current_node = self.step_by_step_for_node(self._len - 1)
+            self.linked_nodes(current_node, append_node)
+            self.tail = append_node
 
-    def insert(self, index: int, value: _T) -> None:
-        pass
+        self._len += 1
 
+    def insert(self, index: int, value: Any) -> None:
+        self.index_valid(index)
+        append_node = Node(value)
+        if index == 0:
+            self.linked_nodes(append_node, self.head)
+            self.head = append_node
+
+        elif index >= self._len:
+            self.append(value)
+
+        else:
+            current_node = self.step_by_step_for_node(index - 1)
+            next_node = self.step_by_step_for_node(index)
+            append_node = Node(value)
+            self.linked_nodes(append_node, next_node)
+            self.linked_nodes(current_node, append_node)
+
+        self._len += 1
+
+    def list_of_nodes(self) -> list:
+        """
+        Метод возвращает список нод
+        :return:
+        """
+        return [node for node in self]
+
+    def __str__(self) -> str:
+        return f"{self.list_of_nodes()}"
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.list_of_nodes()})"
 
 class DoubleLinkedList(LinkedList):
     ...
 
 
 if __name__ == "__main__":
-    ...
+    ll_1 = LinkedList([1, 2, 3])
+    print(str(ll_1))
+    print(repr(ll_1))
